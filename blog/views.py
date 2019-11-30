87af91e1c2ab7048b41402133620658ecb701bf3
Your Name <you@example.com>
2019-12-01 00:07:40 +0300

    Comments

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Post
-from .forms import PostForm
 from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+	post = get_object_or_404(Post, pk=pk)
+	# Список активных комментариев для этой статьи.
+	comments = post.comments.filter(active=True)
+	new_comment = None
+	if request.method == 'POST':
+		# Пользователь отправил комментарий.
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			# Создаем комментарий, но пока не сохраняем в базе данных.
+			new_comment = comment_form.save(commit=False)
+			# Привязываем комментарий к текущей статье.
+			new_comment.post = post
+			# Сохраняем комментарий в базе данных.
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+	return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
 def post_new(request):
     if request.method == "POST":
